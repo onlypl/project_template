eca08044334036aec5136cd9bcf643d91b3b37d1
Lucas <teaqqc@gmail.com>
2025-04-09 16:07:53 +0800

    网络权限配置

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,43 @@
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+// 获取时间戳的工具函数 修改APK包名
+fun getTimestamp(): String = SimpleDateFormat("yyyyMMddHHmm").format(Date())
+android.applicationVariants.all {
+        val variant = this
+        variant.outputs
+            .filterIsInstance<com.android.build.gradle.internal.api.BaseVariantOutputImpl>()
+            .forEach { output ->
+                val outputFileName = "app-${variant.name}-v${variant.versionName}-${getTimestamp()}.apk"
+                output.outputFileName = outputFileName
+            }
+    }
 
 android {
     namespace = "com.template.project_template"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
+
+
+
+    signingConfigs {
+        create("packJKS"){
+            keyAlias = "key" // 别名
+            keyPassword = "pl7611346" // 密码
+            storeFile = file("/Users/onlypl/key.jks")//file("${rootDir.absolutePath}/keystore/key.jks") //file("/Users/onlypl/key.jks") // 存储keystore或者是jks文件的路径
+            storePassword = "pl7611346" // 存储密码
+          //  enableV1Signing = true
+         //   enableV2Signing = true
+        }
+
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -31,10 +60,38 @@ android {
     }
 
     buildTypes {
+//        release {
+//            // TODO: Add your own signing config for the release build.
+//            // Signing with the debug keys for now, so `flutter run --release` works.
+//            signingConfig = signingConfigs.getByName("debug")
+//        }
+
+        // 通过前面配置的签名信息对应的标识符：packJKS拿到签名的配置信息
+        // 保存在mySignConfig中，分别在debug和release中配置上就行了
+        val mySignConfig = signingConfigs.getByName("packJKS")
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+           // isDebuggable = true
+            isMinifyEnabled = true
+            isShrinkResources  = true
+          //  isCrunchPngs = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            // 配置release 的签名信息
+            signingConfig = mySignConfig
+        }
+
+        debug {
+         ///   isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources  = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            // 配置debug的签名信息
+            signingConfig = mySignConfig
         }
     }
 }
