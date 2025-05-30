c8b18e71e7649463caea6e46af442c9f9f3ffdae
Luke <luke@filbetph.com>
2025-05-30 20:20:44 +0800

    根目录的根目录

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
             keyAlias = "key" // 别名
             keyPassword = "pl7611346" // 密码
             //${rootDir.absolutePath}/
-            storeFile =  file("../key.jks")// file("../key.jks")//file("${rootDir.absolutePath}/keystore/key.jks") //file("/Users/onlypl/key.jks") // 存储keystore或者是jks文件的路径
+            storeFile =  file("../../key.jks")// file("../key.jks")//file("${rootDir.absolutePath}/keystore/key.jks") //file("/Users/onlypl/key.jks") // 存储keystore或者是jks文件的路径
             storePassword = "pl7611346" // 存储密码
           //  enableV1Signing = true
          //   enableV2Signing = true
