96dfb7791c47b8bfe4527bff4b267a4810d6368d
Luke <luke@filbetph.com>
2025-05-30 17:56:46 +0800

    build

---
+++ build/app/generated/source/buildConfig/baiduDev/debug/com/template/project_template/BuildConfig.java
@@ -0,0 +1,19 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.template.project_template;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.template.channelbaidu.BAIDU.DEV";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "baiduDev";
+  public static final String FLAVOR_market = "baidu";
+  public static final String FLAVOR_environment = "dev";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Field from product flavor: dev
+  public static final String API_ENV = "DEV";
+  // Field from product flavor: baidu
+  public static final String MARKET = "BAIDU";
+}
