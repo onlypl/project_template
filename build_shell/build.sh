a9094aed761a572c1cbe9ac17d86a0bcb4b9f437
Luke <luke@filbetph.com>
2025-05-30 17:40:41 +0800

    修复脚本

--- build_shell/build.sh
+++ build_shell/build.sh
@@ -16,25 +16,25 @@ echo "[～～～～～channel_low]：${channel_low}"
 if [ ! -n "$platform" ]; then
   echo 'all'
   flutter build apk --flavor ${channel} --dart-define=CHANNEL=${channel}
-  mv ../build/app/outputs/flutter-apk/app-${channel_low}-release.apk ../output_dir/app-${channel}-release.apk
+  mv ./build/app/outputs/flutter-apk/app-${channel_low}-release.apk ../output_dir/app-${channel}-release.apk
   flutter build ios --release
   flutter build ipa --flavor ${channel} --dart-define=CHANNEL=${channel}
-  mv ../build/ios/archive/${channel}.xcarchive ../output_dir/${channel}.xcarchive
+  mv ./build/ios/archive/${channel}.xcarchive ../output_dir/${channel}.xcarchive
 else
   if [ $platform == 'android' ]; then
     flutter build apk --flavor ${channel} --dart-define=CHANNEL=${channel}
-    mv ../build/app/outputs/flutter-apk/app-${channel_low}-release.apk ../output_dir/app-${channel}-release.apk
+    mv ./build/app/outputs/flutter-apk/app-${channel_low}-release.apk ../output_dir/app-${channel}-release.apk
   elif [ $platform == 'ios' ]; then
     flutter build ios --release
     flutter build ipa --flavor ${channel} --dart-define=CHANNEL=${channel}
-    mv ../build/ios/archive/${channel}.xcarchive ../output_dir/${channel}.xcarchive
+    mv ./build/ios/archive/${channel}.xcarchive ../output_dir/${channel}.xcarchive
   else
     echo 'all'
     flutter build apk --flavor ${channel} --dart-define=CHANNEL=${channel}
-    mv ../build/app/outputs/flutter-apk/app-${channel_low}-release.apk ../output_dir/app-${channel}-release.apk
+    mv ./build/app/outputs/flutter-apk/app-${channel_low}-release.apk ../output_dir/app-${channel}-release.apk
     flutter build ios --release
     flutter build ipa --flavor ${channel} --dart-define=CHANNEL=${channel}
-    mv ../build/ios/archive/${channel}.xcarchive ../output_dir/${channel}.xcarchive
+    mv ./build/ios/archive/${channel}.xcarchive ../output_dir/${channel}.xcarchive
   fi
 fi
 
